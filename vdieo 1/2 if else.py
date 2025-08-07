92c3f37d7269ae36c812211ff0d50dd9ab428faa
Mr pranay-git <pranaymanusmare2@gmail.com>
2025-08-07 09:00:29 +0530

    quesation 2 if else

---
+++ vdieo 1/2 if else.py
@@ -0,0 +1,12 @@
+items = input("choose the flavour you want to order \n 1. chocolate \n 2. vanilla \n 3. strawberry \n")
+
+if (items == "1"):
+    print("your order of choclate icecream is placed")
+
+elif(items == "2"):
+    print("your order of vanilla icecream is placed")
+
+elif(items == '3'):
+    print("your order of strawberry icecream is placed")
+else:
+    print("this input is not accepted or flavour is not available")
