0b22177aece237acabc2f15a389c27d19aa5d533
Mr pranay-git <pranaymanusmare2@gmail.com>
2025-08-08 23:26:39 +0530

    question 3 for loop is added

---
+++ vdieo 1/3 for loop.py
@@ -0,0 +1,17 @@
+# for i in range(1,10):
+#     print(i)
+
+
+# while loop in python
+# i = 1
+# while (i<10):
+#     print(i)
+#     i = i+1
+
+
+
+# use of contine in loop
+for i in range(1,20):
+    if (i== 10 or i==5 or i== 12):
+        continue
+    print(i)
