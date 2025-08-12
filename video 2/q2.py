2384c1629bfd0371d4b222369572e5df4846f8ff
Mr pranay-git <pranaymanusmare2@gmail.com>
2025-08-12 23:19:24 +0530

    accept gender and greet them quesation 2 solved

---
+++ video 2/q2.py
@@ -0,0 +1,19 @@
+# 2)Accept the gender from the user as char and print the
+# respective greeting message
+# Ex - Good Morning Sir (on the basis of gender)
+
+Gender=(input("what is your Gender \n Male \n Female \n "))
+
+if(Gender == "Male" or "male"):
+    print("Hello Sir")
+
+elif Gender == "Female" or "female" :
+    print("Hello Mam")
+else:
+    print("Gender is not acceptable")
+
+
+
+
+
+
