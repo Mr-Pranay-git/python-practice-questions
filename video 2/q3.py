a0c260886c21a23df307c000fe22844456895b96
Mr pranay-git <pranaymanusmare2@gmail.com>
2025-08-13 23:32:54 +0530

    quesation 3 added-accept integer and check even or not

---
+++ video 2/q3.py
@@ -0,0 +1,10 @@
+# 3)Accept an integer and check whether it is an even number
+# or odd
+
+number = int(input("Enter the number"))
+
+
+if number % 2 == 0:
+    print("the number is Even \n")
+else :
+    print("the number is not Even \n")
