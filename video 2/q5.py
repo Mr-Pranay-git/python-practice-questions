94d4a2d902c270fd6d5fde7aed1a3894e830d862
Mr pranay-git <pranaymanusmare2@gmail.com>
2025-08-18 00:00:23 +0530

    question 5 of video 2 added

---
+++ video 2/q5.py
@@ -0,0 +1,9 @@
+# 5)Accept a year and check if it a leap year or not
+
+year = int(input("Enter the year"))
+
+if year% 4 == 0 and year % 100 != 0 or year % 400 == 0:
+    print(year,"is a leap year")
+else:
+    print(year,"is not a leap year")
+
