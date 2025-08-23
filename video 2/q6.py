14f900fdc93d0a303e0a5cbc330060fe230f5523
Mr pranay-git <pranaymanusmare2@gmail.com>
2025-08-23 23:18:43 +0530

    question 6 of video 2 added

---
+++ video 2/q6.py
@@ -0,0 +1,8 @@
+# 6)Accept an English alphabet from user and check if it is a  consonant or a vowel
+
+z = input("please give a character")
+
+if z in "aeiouAEIOU":
+    print(f" {z} is a vowel ")
+else : 
+    print(f"{z} is not a vowel")
