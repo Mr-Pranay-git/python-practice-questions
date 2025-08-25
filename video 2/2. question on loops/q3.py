8fff01916f5086b3a879fb8b1df7acba3ea2f10d
Mr pranay-git <pranaymanusmare2@gmail.com>
2025-08-26 00:05:58 +0530

    q3 in video 2 added

---
+++ video 2/2. question on loops/q3.py
@@ -0,0 +1,9 @@
+# take a number as input and print the table
+
+a = int(input("Enter the number \n "))
+
+print("your table starts from here")
+for i in range(a, (a*10)+1,a):
+    print(i)
+    
+    
