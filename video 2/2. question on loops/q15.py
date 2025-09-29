ef8f732bb661ddb905d1b8cb98aa40a61c3fa9f9
Mr pranay-git <pranaymanusmare2@gmail.com>
2025-09-29 23:30:36 +0530

    15th question added

---
+++ video 2/2. question on loops/q15.py
@@ -0,0 +1,16 @@
+# star pyramid pattern Right Triangle- Star
+
+n = 6
+
+for i in range(1,n+1):
+    for j in range(1,i+1):
+     print("*", end="")
+    print()
+
+
+# extra questions
+# 11) strong number
+# 12) Tell all the strong number in a range
+# 13) tell all the prime numbers in a range
+# 14) print the fabionacchi series
+# 15) Right Triangle - Star
