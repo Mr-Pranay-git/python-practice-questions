848235c75b7ffa56ddcb8779074b2149667f5ce6
Mr pranay-git <pranaymanusmare2@gmail.com>
2026-02-26 23:57:55 +0530

    done question on pallindrome

---
+++ video 4/1 Questions on strings/q5.py
@@ -0,0 +1,11 @@
+# 5)Check string is Pallindrome or not
+
+a = 'naman'
+b = ''
+
+for i in range(len(a),-1,-1):
+    b = b+ a[i]
+if b==a:
+    print('the entered string is pallindrome')
+else:
+    print('not a palandrome')
