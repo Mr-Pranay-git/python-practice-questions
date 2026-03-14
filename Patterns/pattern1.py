9d35c0f09a92f4d5b511d3f778997a4be1d35284
Mr pranay-git <pranaymanusmare2@gmail.com>
2026-03-15 00:35:33 +0530

    square pattern

---
+++ Patterns/pattern1.py
@@ -0,0 +1,11 @@
+# *****
+# *****
+# *****
+
+rows = int(input("Enter rows")) #5
+columbs = int(input("columbs")) #5
+
+for i in range(rows):
+    for j in range(columbs):
+        print("*", end=" ")
+    print()
