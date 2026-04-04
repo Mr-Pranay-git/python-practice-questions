9baabb405e3d62ce08a3fb090e0adfb4874e06c5
Mr pranay-git <pranaymanusmare2@gmail.com>
2026-04-04 23:53:08 +0530

    question on loops

--- video 2/2. question on loops/q1.py
+++ video 2/2. question on loops/q1.py
@@ -1,6 +1,6 @@
 # 1)Print natural number up to n.
 
-number = input("Please tell your  number \n")
+number = int(input("Please tell your  number \n"))
 
-for i in range (1,n):
+for i in range (1,number):
     print(i)
